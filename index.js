c3fb2f3e1f6c8d986bcbc64a14ce5a598f0b814d
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-09 09:57:51 +0600

    Localhost set up and working

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hello, Mission SCIC');
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is running on ${port}`);
+})
